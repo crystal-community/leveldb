fa266a2de1994672115644548de1f4673aa564b2
Sergey Potapov <blake131313@gmail.com>
2016-07-09 22:53:50 +0200

    Clean lib

--- src/leveldb/lib_leveldb.cr
+++ src/leveldb/lib_leveldb.cr
@@ -7,34 +7,24 @@ lib LibLevelDB
     SNAPPY_COMPRESSION
   end
 
+  # DB
   fun leveldb_open(options : Void*, name : UInt8*,  errptr : Void*) : Void*
-
-  fun leveldb_options_create() : Void*
-
-  fun leveldb_writeoptions_create() : Void*
-
   fun leveldb_put(db : Void*, woptions : Void*, key : UInt8*, keylen : UInt64, val : UInt8*, vallen : UInt64, errptr : Void*) : Void
-
-  fun leveldb_options_set_create_if_missing(options : Void*, val : Bool)
-
-  fun leveldb_options_set_compression(options : Void*, val : Compression);
-
-  fun leveldb_readoptions_create() : Void*
-
   fun leveldb_get(db : Void*, roptions : Void*, key : UInt8*, keylen : UInt64, vallen : UInt64*, errptr : Void*) : UInt8*
-
   fun leveldb_delete(db : Void*, woptions : Void*, key : UInt8*, keylen : UInt64, errptr : Void*) : Void
-
-  fun leveldb_free(ptr : Void*)
-
   fun leveldb_close(db : Void*)
-
   fun leveldb_destroy_db(options : Void*, name : UInt8*, errptr : Void*)
 
+  # Options
+  fun leveldb_options_create() : Void*
+  fun leveldb_options_set_create_if_missing(options : Void*, val : Bool)
+  fun leveldb_options_set_compression(options : Void*, val : Compression)
+  fun leveldb_writeoptions_create() : Void*
+  fun leveldb_readoptions_create() : Void*
+
+  # Snapshot
   fun leveldb_create_snapshot(db : Void*) : Void*
-
   fun leveldb_readoptions_set_snapshot(roptions : Void*, snapshot : Void*)
-
   fun leveldb_release_snapshot(db : Void*, snapshot : Void*)
 
   # Iterator
@@ -48,4 +38,7 @@ lib LibLevelDB
   fun leveldb_iter_seek_to_last(iterator : Void*)
   fun leveldb_iter_seek(iterator : Void*, key : UInt8*, klen : SizeT);
   fun leveldb_iter_valid(iterator : Void*) : Bool
+
+  # Misc
+  fun leveldb_free(ptr : Void*)
 end
