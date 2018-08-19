8f6c847270b0262bb73de8426957024c9d06522f
Diego <dgub@pm.me>
2018-08-19 23:44:22 +0200

    added batch example and README:w

--- spec/leveldb/batch_spec.cr
+++ spec/leveldb/batch_spec.cr
@@ -5,20 +5,31 @@ describe LevelDB::Batch do
     FileUtils.rm_r(TEST_DB) if Dir.exists?(TEST_DB)
     db = LevelDB::DB.new(TEST_DB)
 
-    batch = LevelDB::Batch.new
-    batch.put("a", "2")
-    batch.put("b", "3")
-    batch.put("c", "4")
-    batch["d"] = "5"
-    batch.delete("b")
+    begin
+      batch = LevelDB::Batch.new
+      batch.put("a", "2")
+      batch.put("b", "3")
+      batch.put("c", "4")
+      batch["d"] = "5"
+      batch.delete("b")
 
-    db.write(batch)
+      db.write(batch)
 
-    db.get("b").should eq nil
-    db.get("a").should eq "2"
-    db.get("c").should eq "4"
-    db.get("d").should eq "5"
+      db.get("b").should eq nil
+      db.get("a").should eq "2"
+      db.get("c").should eq "4"
+      db.get("d").should eq "5"
 
-    db.close
+      batch = LevelDB::Batch.new
+      batch.put("a", "20")
+      batch.put("b", "1")
+      db.write(batch)
+
+      db.get("b").should eq "1"
+      db.get("a").should eq "20"
+
+    ensure
+      db.close
+    end
   end
 end
